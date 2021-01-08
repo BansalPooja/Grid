1ab15f87c0f8b188278c2cef60be34b66e0fd228
pbansaltrekbikes <pooja_bansal@trekbikes.com>
2021-01-09 00:16:27 +0530

    addin

---
+++ src/views/About.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <TabulatorComponent
+      :dataMas="data"
+      :options="options"
+    />
+  </div>
+</template>
+
+<script>
+import TabulatorComponent from 'tabulator-vue';
+import 'tabulator-vue/dist/tabulator-vue.css';
+
+export default {
+  name: '#app',
+  components: {
+    TabulatorComponent
+  },
+  data () {
+    return {
+      data: [
+        {
+          name: 'Tom',
+          age: 14
+        },
+        {
+          name: 'Jack',
+          age: 11
+        },
+         {
+          name: 'May',
+          age: 15
+        },
+         {
+          name: 'Paul',
+          age: 18
+        },
+         {
+          name: 'David',
+          age: 10
+        },
+      ],
+      options: {
+        columns: [
+            {
+              title: 'Name',
+              field: 'name',
+              sorter: 'string',
+              width: 200,
+              editable: false,
+            },
+            {
+              title: 'Age',
+              field: 'age',
+            },
+        ],
+        autoResize: false,
+        columnHeaderVertAlign:"center",
+      },
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  
+
+    <ag-grid
+    class="ag-theme-alpine"
+    :column-defs="cols"
+    :row-data="rowData"
+    :column-types="columnTypes"
+    :dom-layout="domLayout"
+    :auto-fit="autoFit"
+    @first-data-rendered="onFirstDataRendered"
+  >
+  </ag-grid>
+
+<grid :cols="cols" :rows="rows" :sort=true :pagination=true :search=true></grid>
+</template>
+<script>
+//import {Grid} from 'gridjs-vue';
+import { agGrid } from '@ag-grid-enterprise/core';
+import {SetFilterModule} from '@ag-grid-enterprise/set-filter';
+export default {
+  components: {
+    Grid
+  },
+  data() {
+      return {
+        cols: ['Make', 'Model', 'Year', 'Color'],
+        rows: [
+          ['Ford', 'Fusion', '2011', 'Silver'],
+          ['Chevrolet', 'Cruz', '2018', 'White']
+        ]
+      }
+    }
+}
+</script>
